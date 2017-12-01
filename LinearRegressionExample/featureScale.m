d910bc3c4649aaf99588c10593d79b9252591868
Jaleel Savoy <jaleelwsavoy@outlook.com>
2017-12-01 15:26:48 -0600

    Update featureScale.m

--- LinearRegressionExample/featureScale.m
+++ LinearRegressionExample/featureScale.m
@@ -1,7 +1,7 @@
 function [X_scale, X_max, X_min] = featureScale(X)
   X_scale = X;
   X_max = zeros(1, size(X,2));
-  X_max = zeros(1, size(X,2));
+  X_min = zeros(1, size(X,2));
   
   %Rescaling
   X_max = max(X);
